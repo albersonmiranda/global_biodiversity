50acfd6adbeb29b79c1911d5303f351f23e86153
Alberson Miranda <albersonmiranda@hotmail.com>
2024-11-21 03:50:36 -0300

    fix: port on app

--- src/app.R
+++ src/app.R
@@ -7,7 +7,7 @@ source("R/server.R")
 
 # Specify the application port
 options(shiny.host = "0.0.0.0")
-options(shiny.port = 8180)
+options(shiny.port = 8080)
 
 # run app
 shiny::shinyApp(ui = app_ui, server = app_server)

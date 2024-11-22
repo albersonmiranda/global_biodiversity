0198cede82a8ea2e98319282c49a7293bbaee63b
Alberson Miranda <albersonmiranda@hotmail.com>
2024-11-22 19:07:22 -0300

    chore: cleanup server

--- src/R/server.R
+++ src/R/server.R
@@ -9,16 +9,9 @@ source("R/app_map.R")
 app_server <- function(input, output, session) {
   # Home module
   app_home_server("home", session)
-
-  # db connection
   con <- dbConnect(duckdb::duckdb(), dbdir = "data/db.duckdb", read_only = TRUE)
   onStop(function() dbDisconnect(con))
-
-  # dataset module
   app_dataset_server("dataset", con)
-
-  # Map module
   app_map_server("maps", con)
-
   app_timeline_server("timeline", con)
 }
